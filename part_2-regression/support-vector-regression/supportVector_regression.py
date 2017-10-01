ac6e2e7b40529ec7eb8338ef7ddcd1766d0b4809
Nikhil <ssvnikhil03@gmail.com>
2017-10-01 17:07:13 -0500

    Support Vector Regression Machine Learning Model

---
+++ part_2-regression/support-vector-regression/supportVector_regression.py
@@ -0,0 +1,53 @@
+# Support Vector Regression Models
+
+# Import the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+#   Importing Dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+#Splitting the dataset into Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = .2, random_state =0)"""
+
+#   Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+
+#   Fitting the Regression to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+
+#   Predicting a new result with Regression Model
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
+
+
+#   Visualizing the Support Vector Regression Model results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Support Vector Regression Model)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+
+#   Visualizing the Support Vector Regression Model results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Support Vector Regression Model smoother curve with high resolution)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+
